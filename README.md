0ffca68a4d4a982eba3243d0455be6607d2ca17e
Radu <radu.groza@bistone.eu>
2021-02-05 10:02:00 +0200

    plugin unit tests

--- README.md
+++
@@ -1,9 +0,0 @@
-# wordpress-plugin-unit-tests
-
-This will setup everything needed for writing WP plugin unit tests
-
-###How to install
-
-- Drop the php file in a bin folder inside your WP plugin
-- Run this php file from a CLI
-
---
+++ my-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: My plugin test
+ * Plugin URI: https://dummy.com
+ * Description: Test shit out
+ * Author URI: https://dummy.com
+ * Version: 0.0.1
+ * Author: <a href="https://dummy.com">Dummy author</a>
+ * Text Domain: my-plugin
+ */
+
