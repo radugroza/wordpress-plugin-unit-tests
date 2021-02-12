976ddc84bbdbd80598aac99a2a36ca33f205d751
Radu <radu.groza@bistone.eu>
2021-02-12 08:46:43 +0200

    hook send results

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -8,13 +8,16 @@
 /**
  * Sample test case.
  */
-class SampleTest extends WP_UnitTestCase {
+class SampleTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * A single example test.
+     */
+    public function test_sample()
+    {
+        // Replace this with some actual testing code.
+        $this->assertTrue(true);
 
-	/**
-	 * A single example test.
-	 */
-	public function test_sample() {
-		// Replace this with some actual testing code.
-		$this->assertTrue( true );
-	}
+        $this->assertEquals('expected', 'actual', 'This should be failing');
+    }
 }
